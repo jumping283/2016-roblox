24df625ef27c5a9ae8918f593f78f0721e6f3ab3
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-03 10:36:59 -0500

    add white background

--- components/myAccount2016/index.js
+++ components/myAccount2016/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { createUseStyles } from "react-jss";
 import { getInventoryPrivacy, getTradePrivacy, getTradeValue } from "../../services/accountSettings";
 import { getUserInfo } from "../../services/users";
 import AuthenticationStore from "../../stores/authentication";
@@ -8,7 +9,15 @@ import PrivacySettings from "./components/privacySettings";
 import Tabs from "./components/tabs";
 import MyAccountStore from "./stores/myAccountStore";
 
+const useStyles = createUseStyles({
+  settingsRow: {
+    background: '#f2f2f2',
+  },
+})
+
 const MyAccount = props => {
+  const s = useStyles();
+
   const store = MyAccountStore.useContainer();
   const auth = AuthenticationStore.useContainer();
   useEffect(() => {
@@ -26,7 +35,7 @@ const MyAccount = props => {
   if (auth.isPending || !auth.userId) return null;
   return <div className='container'>
     <ModalHandler></ModalHandler>
-    <div className='row'>
+    <div className={'row pb-2 ' + s.settingsRow}>
       <div className='col-12'>
         <h1 className='mt-0 mb-0'>My Settings</h1>
       </div>
