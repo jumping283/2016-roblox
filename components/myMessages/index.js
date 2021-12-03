b0805b9d81111c1da7cd70e27815ab0a8f09f81b
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-03 11:06:24 -0500

    add white background; hide overflow;

--- components/myMessages/index.js
+++ components/myMessages/index.js
@@ -1,39 +1,52 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
 import MyMessagesStore from "../../stores/myMessages";
 import AdBanner from "../ad/adBanner";
 import OldVerticalTabs from "../oldVerticalTabs";
 import LargeMessage from "./components/largeMessage";
 import MessageRow from "./components/messageRow";
 
+const useStyles = createUseStyles({
+  messagesContainer: {
+    background: '#fff',
+    padding: '4px 8px',
+    overflow: 'hidden',
+  }
+})
+
 const MyMessages = props => {
+  const s = useStyles();
+
   const store = MyMessagesStore.useContainer();
   return <div className='container'>
     <AdBanner></AdBanner>
-    <div className='row mt-2'>
-      <div className='col-12'>
-        {
-          store.highlightedMessage ? <LargeMessage {...store.highlightedMessage}></LargeMessage> :
+    <div className={s.messagesContainer}>
+      <div className='row mt-2'>
+        <div className='col-12'>
+          {
+            store.highlightedMessage ? <LargeMessage {...store.highlightedMessage}></LargeMessage> :
 
-            <OldVerticalTabs options={[
-              {
-                name: 'Inbox',
-                element: <MessageRow tab='inbox'></MessageRow>,
-              },
-              {
-                name: 'Sent',
-                element: <MessageRow tab='sent'></MessageRow>,
-              },
-              {
-                name: 'Notifications',
-                element: <MessageRow tab='notifications'></MessageRow>,
-                count: store.notifications ? store.notifications.collection.length : undefined,
-              },
-              {
-                name: 'Archive',
-                element: <MessageRow tab='archive'></MessageRow>,
-              },
-            ]}></OldVerticalTabs>
-        }
+              <OldVerticalTabs options={[
+                {
+                  name: 'Inbox',
+                  element: <MessageRow tab='inbox'></MessageRow>,
+                },
+                {
+                  name: 'Sent',
+                  element: <MessageRow tab='sent'></MessageRow>,
+                },
+                {
+                  name: 'Notifications',
+                  element: <MessageRow tab='notifications'></MessageRow>,
+                  count: store.notifications ? store.notifications.collection.length : undefined,
+                },
+                {
+                  name: 'Archive',
+                  element: <MessageRow tab='archive'></MessageRow>,
+                },
+              ]}></OldVerticalTabs>
+          }
+        </div>
       </div>
     </div>
   </div>
