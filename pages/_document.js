ec5450e0d104a22b07f5227f0d78f5fe4a1030f5
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-03 11:41:05 -0500

    remove render() method;

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 import { SheetsRegistry, JssProvider, createGenerateId } from 'react-jss'
+import { getTheme, themeType } from '../services/theme'
 
 export default class JssDocument extends Document {
   static async getInitialProps(ctx) {
@@ -27,17 +28,5 @@ export default class JssDocument extends Document {
         </>
       ),
     }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body style={{ background: 'url(/img/Unofficial/obc_theme_2016_bg.png) repeat-x #222224' }}>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
   }
 }
