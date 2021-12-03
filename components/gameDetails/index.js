751d741564c63f35331fe73ecde4a6e1a8814394
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-03 11:06:31 -0500

    add white background

--- components/gameDetails/index.js
+++ components/gameDetails/index.js
@@ -1,5 +1,6 @@
 // reference: https://web.archive.org/web/20150228101148mp_/http://www.roblox.com/Work-at-a-Pizza-Place-place?id=192800
 import React, { useEffect } from "react";
+import { createUseStyles } from "react-jss";
 import AdBanner from "../ad/adBanner";
 import AdSkyscraper from "../ad/adSkyscraper";
 import Comments from "../catalogDetailsPage/components/comments";
@@ -9,8 +10,18 @@ import GameOverview from "./components/gameOverview";
 import GameServers from "./components/gameServers";
 import GameDetailsStore from "./stores/gameDetailsStore"
 
+const useStyles = createUseStyles({
+  gameContainer: {
+    backgroundColor: '#fff',
+    padding: '4px 8px',
+    overflow: 'hidden',
+  }
+})
+
 const GameDetails = props => {
   const { details } = props;
+  const s = useStyles();
+
   const store = GameDetailsStore.useContainer();
   useEffect(() => {
     store.setDetails(details);
@@ -19,30 +30,32 @@ const GameDetails = props => {
   if (!store.details) return null;
   return <div className='container'>
     <AdBanner></AdBanner>
-    <div className='row mt-2'>
-      <div className='col-12 col-lg-10'>
-        <GameOverview></GameOverview>
-        <div className='row mt-4'>
-          <div className='col-12'>
-            <OldVerticalTabs options={[
-              {
-                name: 'Recommendations',
-                element: <Recommendations assetId={details.id} assetType={9}></Recommendations>
-              },
-              {
-                name: 'Games',
-                element: <GameServers></GameServers>
-              },
-              {
-                name: 'Commentary',
-                element: <Comments assetId={details.id}></Comments>
-              },
-            ]}></OldVerticalTabs>
+    <div className={s.gameContainer}>
+      <div className='row mt-2'>
+        <div className='col-12 col-lg-10'>
+          <GameOverview></GameOverview>
+          <div className='row mt-4'>
+            <div className='col-12'>
+              <OldVerticalTabs options={[
+                {
+                  name: 'Recommendations',
+                  element: <Recommendations assetId={details.id} assetType={9}></Recommendations>
+                },
+                {
+                  name: 'Games',
+                  element: <GameServers></GameServers>
+                },
+                {
+                  name: 'Commentary',
+                  element: <Comments assetId={details.id}></Comments>
+                },
+              ]}></OldVerticalTabs>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='d-none d-lg-flex col-2'>
-        <AdSkyscraper></AdSkyscraper>
+        <div className='d-none d-lg-flex col-2'>
+          <AdSkyscraper></AdSkyscraper>
+        </div>
       </div>
     </div>
   </div>
