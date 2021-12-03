8348469ca093b073c6cd7a1d6bf671f992f099bb
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-03 15:32:52 -0500

    add constants

---
+++ components/develop/constants.js
@@ -0,0 +1,77 @@
+
+const developerPages = [
+  {
+    name: 'Games',
+    url: '/develop?View=0'
+  },
+  {
+    name: 'Places',
+    url: '/develop?View=9',
+    disabled: true,
+  },
+  {
+    name: 'Models',
+    url: '/develop?View=10',
+    disabled: true,
+  },
+  {
+    name: 'Decals',
+    url: '/develop?View=13',
+    disabled: true,
+  },
+  {
+    name: 'Badges',
+    url: '/develop?View=21',
+    disabled: true,
+  },
+  {
+    name: 'Passes',
+    url: '/develop?View=100',
+    disabled: true,
+  },
+  {
+    name: 'Audio',
+    url: '/develop?View=3',
+  },
+  {
+    name: 'Animations',
+    url: '/develop?View=24',
+    disabled: true,
+  },
+  {
+    name: 'Meshes',
+    url: '/develop?View=40',
+    disabled: true,
+  },
+  {
+    name: 'User Ads',
+    url: '/develop?view=101',
+    disabled: true,
+  },
+  {
+    name: 'Sponsored Games',
+    url: '/develop?View=102',
+    disabled: true,
+  },
+  {
+    name: 'Shirts',
+    url: '/develop?View=11',
+  },
+  {
+    name: 'T-Shirts',
+    url: '/develop?View=2',
+  },
+  {
+    name: 'Pants',
+    url: '/develop?View=12'
+  },
+  {
+    name: 'Plugins',
+    url: '/develop?View=38',
+    disabled: true,
+  },
+];
+
+export {
+  developerPages,
+}
