911c08ac4dffa2992ce32c4a4026ca75bc4f15a0
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-30 12:32:20 -0500

    add viewport meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -57,6 +57,7 @@ function RobloxApp({ Component, pageProps }) {
       <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&amp;display=swap" rel="stylesheet" />
       <title>{pageProps.title || 'ROBLOX'}</title>
       <link rel='icon' type="image/vnd.microsoft.icon" href='/favicon.ico' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
     </Head>
     <AuthenticationStore.Provider>
       <LoginModalStore.Provider>
