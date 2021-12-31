480b0be06f0e1e065e4244fbb4b35111652541f1
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-30 21:30:27 -0500

    add ads support;

--- components/develop/components/assetList.js
+++ components/develop/components/assetList.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { createUseStyles } from "react-jss"
 import { itemNameToEncodedName } from "../../../services/catalog";
 import GearDropdown from "../../gearDropdown";
+import AssetListAdEntry from "./assetListAdEntry";
 import AssetListCatalogEntry from "./assetListCatalogEntry";
 import AssetListGameEntry from "./assetListGameEntry";
 
@@ -17,14 +18,22 @@ const useStyles = createUseStyles({
     borderBottom: '1px solid #f2f2f2',
     paddingBottom: '4px',
   },
+  gearDropdownWrapper: {
+    marginBottom: '-1rem',
+  },
 });
 
 const AssetEntry = props => {
   const s = useStyles();
   const isPlace = props.assetType === 9;
+  const isAd = props.ad !== undefined && props.target !== undefined;
 
   const assetUrl = `/${itemNameToEncodedName(props.name)}-${isPlace ? 'game' : 'item'}?id=${props.assetId}`;
   const url = isPlace ? `/universes/configure?id=${props.universeId}` : assetUrl;
+
+  const imageAssetId = isAd ? props.ad.advertisementAssetId : props.assetId;
+  // todo: figure out better way to do this
+  const [runMenuOpen, setRunMenuOpen] = useState(false);
 
   const gearOptions = [
     isPlace && {
@@ -54,13 +63,21 @@ const AssetEntry = props => {
     isPlace && {
       name: 'separator',
     },
-    !isPlace && {
+    !isAd && !isPlace && {
       name: 'Configure',
       url: `/catalog/configure?id=${props.assetId}`,
     },
-    {
+    !isAd && {
       name: 'Advertise',
       url: `/user-ads/create?targetId=${props.assetId}&targetType=Asset`,
+    },
+    isAd && {
+      name: 'Run',
+      onClick: e => {
+        e.preventDefault();
+        console.log('run ad');
+        setRunMenuOpen(!runMenuOpen);
+      },
     },
     isPlace && {
       name: 'separator',
@@ -77,16 +94,19 @@ const AssetEntry = props => {
 
   return <div className={'row ' + s.row}>
     <div className='col-2'>
-      <img className={s.image} src={`/Thumbs/Asset.ashx?width=100&height=100&assetId=${props.assetId}`}></img>
+      <img className={s.image} src={`/Thumbs/Asset.ashx?width=100&height=100&assetId=${imageAssetId}`}></img>
     </div>
-    <div className='col-4 ps-0'>
+    <div className='col-9 ps-0'>
       <p className='mb-0'><a href={url}>{props.name}</a></p>
-      {props.assetType === 9 ? <AssetListGameEntry url={assetUrl} startPlaceName={props.name}></AssetListGameEntry> : <AssetListCatalogEntry created={props.created}></AssetListCatalogEntry>}
+      {
+        isAd ? <AssetListAdEntry ad={props.ad} target={props.target} runMenuOpen={runMenuOpen} setRunMenuOpen={setRunMenuOpen}></AssetListAdEntry>
+          : props.assetType === 9 ?
+            <AssetListGameEntry url={assetUrl} startPlaceName={props.name}></AssetListGameEntry>
+            : <AssetListCatalogEntry created={props.created}></AssetListCatalogEntry>
+      }
     </div>
-    <div className='col-6'>
-      <div className='float-right'>
-        <GearDropdown boxDropdownRightAmount={0} options={gearOptions.filter(v => !!v)}></GearDropdown>
-      </div>
+    <div className='col-1'>
+      <GearDropdown boxDropdownRightAmount={0} options={gearOptions.filter(v => !!v)}></GearDropdown>
     </div>
   </div>
 }
@@ -96,7 +116,7 @@ const AssetList = props => {
     <div className='col-12'>
       {
         props.assets.map(v => {
-          return <AssetEntry key={v.assetId} {...v}></AssetEntry>
+          return <AssetEntry key={v.assetId || v.ad.id} {...v}></AssetEntry>
         })
       }
     </div>
