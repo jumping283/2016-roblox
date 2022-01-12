090512ddceecd6a6b50d07cf4b8410ee713c8247
beak <27046219+floatzeI@users.noreply.github.com>
2022-01-11 20:00:47 -0500

    fix path seperator bug on windows

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const configPath = path.join(__dirname, './config.json');
+const configPath = path.join(__dirname, path.sep + 'config.json');
+if (!fs.existsSync(configPath)) {
+  throw new Error('Configuration could not be found at location: ' + configPath);
+}
 const config = JSON.parse(fs.readFileSync(configPath).toString('utf-8'));
 
 module.exports = {
