037991d1033bfa50d7c5be10be2adf4fed17c284
beak <27046219+floatzeI@users.noreply.github.com>
2022-01-29 13:37:29 -0500

    add setupTests

---
+++ setupTests.js
@@ -0,0 +1,9 @@
+// setupTests.js
+
+import 'regenerator-runtime/runtime'
+import Enzyme from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+})
