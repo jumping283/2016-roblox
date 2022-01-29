d0e406633056032611fa6cd0868875dd3e15274d
beak <27046219+floatzeI@users.noreply.github.com>
2022-01-29 17:18:25 -0500

    fix bug where "Pending approval from you" was shown on outbound trades

--- components/myMoney/lib/getStatusText.js
+++ components/myMoney/lib/getStatusText.js
@@ -1,8 +1,17 @@
-export const getStatusText = (status) => {
-  switch (status) {
+export const getStatusText = (tradeData, tradeType, authenticatedUserId) => {
+  switch (tradeData.status) {
     case 'Open':
+      if (tradeData.user.id === authenticatedUserId || tradeType === 'outbound' || tradeType === 'sent') {
+        let otherUser;
+        if (tradeData.offers) {
+          otherUser = tradeData.offers.find(v => v.user.id !== authenticatedUserId);
+        } else {
+          otherUser = tradeData;
+        }
+        return 'Pending approval from ' + otherUser.user.name;
+      }
       return 'Pending approval from you';
     default:
-      return status;
+      return tradeData.status;
   }
 }
