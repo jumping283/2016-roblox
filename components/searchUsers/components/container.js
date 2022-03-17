4fc98f15f7024bf8d6ca3af7ca863ababbb7af26
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-03-16 20:15:41 -0400

    fix dark theme;

--- components/searchUsers/components/container.js
+++ components/searchUsers/components/container.js
@@ -2,15 +2,23 @@ import searchUsersStore from "../stores/searchUsersStore";
 import {useEffect} from "react";
 import InputRow from "./inputRow";
 import UsersRow from "./usersRow";
+import {createUseStyles} from "react-jss";
+
+const useStyles = createUseStyles({
+  row: {
+    background: '#fff',
+  }
+})
 
 const Container = props => {
   const store = searchUsersStore.useContainer();
+  const s = useStyles();
   useEffect(() => {
     store.setKeyword(props.keyword);
     store.setData(null);
   }, [props]);
 
-  return <div className='row'>
+  return <div className={'row '  +s.row}>
     <div className='col-12'>
       <InputRow />
       <UsersRow />
