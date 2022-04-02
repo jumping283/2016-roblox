cb5053b84016a7b9684de7bf9859438672dac256
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-04-02 17:26:56 -0400

    add accessory support (flag accessoriesEnabled)

--- components/userInventory/components/categorySelection.js
+++ components/userInventory/components/categorySelection.js
@@ -3,6 +3,8 @@ import Dropdown2016 from "../../dropdown2016";
 import Selector from "../../selector";
 import getFlag from "../../../lib/getFlag";
 import UserInventoryStore from "../stores/userInventoryStore";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const useStyles = createUseStyles({
   categoryTitle: {
@@ -30,6 +32,13 @@ const useStyles = createUseStyles({
       borderRight: '4px solid rgb(0, 162, 255)',
     },
   },
+  childSelector: {
+    position: 'absolute',
+    width: '140px',
+    marginTop: '-35px',
+    marginLeft: '143px',
+    zIndex: 4,
+  }
 })
 
 const CategorySelection = props => {
@@ -48,7 +57,44 @@ const CategorySelection = props => {
       name: 'Gears',
       value: 'Gear',
     },
-    {
+    getFlag('accessoriesEnabled', false) ? {
+      name: 'Accessories',
+      value: 'Accessories',
+      children: [
+        {
+          name: 'Hats',
+          value: 'Hat',
+        },
+        {
+          name: 'Hair',
+          value: 'HairAccessory',
+        },
+        {
+          name: 'Face',
+          value: 'FaceAccessory',
+        },
+        {
+          name: 'Neck',
+          value: 'NeckAccessory',
+        },
+        {
+          name: 'Shoulder',
+          value: 'ShoulderAccessory',
+        },
+        {
+          name: 'Front',
+          value: 'FrontAccessory',
+        },
+        {
+          name: 'Back',
+          value: 'BackAccessory',
+        },
+        {
+          name: 'Waist',
+          value: 'WaistAccessory',
+        },
+      ]
+    } : {
       name: 'Hats',
       value: 'Hat',
     },
@@ -105,23 +151,62 @@ const CategorySelection = props => {
       value: 'Package',
     },
   ].filter(v => !!v);
+  const [selected ,setSelected] = useState(() => {
+    // On first load, hide menu if user is on desktop.
+    // This is so that the Accessory side menu doesn't show up.
+    if (typeof window !== 'undefined' && window.innerWidth >= 992) {
+      return null;
+    }
+
+    return options.find(v => v.value === (getFlag('accessoriesEnabled', false) ? 'Accessories' : 'Hats'))
+  });
   const onChange = v => {
+    if (v.children) {
+      setSelected(selected === v ? null : v);
+      return;
+    }
     store.setCategory(v);
   }
+  const onChangeSubCategory = v => {
+    store.setCategory(v);
+  }
+
   return <div className='col-12 col-lg-2'>
     <div className='d-block d-lg-none'>
       <h2 className={s.categoryTitle}>CATEGORY</h2>
-      <Selector options={options} onChange={onChange} value='Hat' />
+      <Selector options={options} onChange={onChange} value={getFlag('accessoriesEnabled', false) ? 'Accessories' : 'Hats'} />
+      {
+        selected && selected.children ? <>
+          <h2 className={s.categoryTitle}>SUBCATEGORY</h2>
+          <Selector options={selected.children} onChange={onChangeSubCategory} value='Hat' /> 
+        </> : null
+      }
     </div>
     <div className='d-none d-lg-block d-xl-block'>
       <div className={s.categoryBgDesktop}>
         <div className='ps-3 pe-3 pt-4'><h2 className={s.categoryTitle}>CATEGORY</h2></div>
         {options.map(v => {
-          const selected = store.category.value === v.value;
-          return <div onClick={e => {
-            onChange(v);
-          }} key={v.value} className={s.selectorOption + ' ' + (selected ? s.selectorOptionSelected : '')}>
-            {v.name}
+          const catSelected = store.category.value === v.value;
+          return <div onMouseEnter={() => {
+            setSelected(v);
+          }}>
+            <div onClick={e => {
+              onChange(v);
+            }} key={v.value} className={s.selectorOption + ' ' + (catSelected ? s.selectorOptionSelected : '')}>
+              {v.name}
+            </div>
+            {selected && selected.children && selected.value === v.value ? <div className={s.categoryBgDesktop + ' ' + s.childSelector}>
+              {selected.children.map(v => {
+                const selected = store.category.value === v.value;
+                return <div onMouseLeave={() => {
+                }} onClick={e => {
+                  onChange(v);
+                  setSelected(null);
+                }} key={v.value} className={s.selectorOption + ' ' + (selected ? s.selectorOptionSelected : '')}>
+                  {v.name}
+                </div>
+              })}
+              </div> : null}
           </div>
         })}
       </div>
