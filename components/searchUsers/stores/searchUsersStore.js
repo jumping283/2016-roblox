adcbea8e0fe2498c3757f774052bcb851a1d34bc
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-04-08 14:17:16 -0400

    add searchUsersAllowNullKeyword; don't search users if keyword is null;

--- components/searchUsers/stores/searchUsersStore.js
+++ components/searchUsers/stores/searchUsersStore.js
@@ -2,14 +2,17 @@ import {createContainer} from "unstated-next";
 import {useEffect, useState} from "react";
 import {searchUsers} from "../../../services/users";
 import {multiGetPresence} from "../../../services/presence";
+import getFlag from "../../../lib/getFlag";
 
 const SearchUsersStore = createContainer(() => {
   const [keyword, setKeyword] = useState(null);
   const [data, setData] = useState(null);
-  const [locked, setLocked] = useState(false);
+  const [locked, setLocked] = useState(true);
   const [presence, setPresence] = useState({});
 
   useEffect(() => {
+    if (!keyword && !getFlag('searchUsersAllowNullKeyword', false))
+      return;
     searchUsers({keyword, limit: 12, offset: 0}).then(d => {
       setData(d);
       const ids = d.UserSearchResults.map(v => v.UserId);
