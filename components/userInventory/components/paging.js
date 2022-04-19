eeaa1bdc476a680f08777f4980be4e1c20b50170
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-04-19 19:23:25 -0400

    move 2016 pagination to its own component;

--- components/userInventory/components/paging.js
+++ components/userInventory/components/paging.js
@@ -1,60 +1,12 @@
 import UserInventoryStore from "../stores/userInventoryStore";
-import {createUseStyles} from "react-jss";
+import Paging from "../../pagination2016";
 
-const useStyles = createUseStyles({
-  buttonWrapper: {
-    margin: '0 auto',
-    display: 'flex',
-    justifyContent: 'center',
-    maxWidth: '300px',
-  },
-  buttonPaginate: {
-    color: '#1a1a1a',
-    background: '#fff',
-    border: '1px solid #dbdbdb',
-    padding: '0px 12px',
-    fontSize: '24px',
-    fontWeight: 600,
-    borderRadius: '4px',
-    marginBottom: 0,
-    height: '34px',
-    cursor: 'pointer',
-  },
-  textCurrentPage: {
-    margin: '8px 16px 0 16px',
-    fontWeight: '300',
-  },
-  buttonPaginateDisabled: {
-    color: '#c3c3c3',
-    cursor: 'default',
-  },
-});
-
-const Paging = props => {
+const InventoryPaging = props => {
   const store = UserInventoryStore.useContainer();
-  const s = useStyles();
-  if (!store.data) return null;
-  const totalDisplay = store.data.TotalItems ? ('of '+ Math.ceil((store.data.TotalItems) / store.limit)) : (store.nextPageAvailable() ? 'of many' : null);
+  if (!store.data)
+    return null;
 
-  return <div className='row'>
-    <div className='col-12'>
-      <div className={s.buttonWrapper}>
-        <div className={s.buttonPaginate + ' ' + (!store.previousPageAvailable() ? s.buttonPaginateDisabled : '')} onClick={() => {
-          if (store.previousPageAvailable())
-            store.loadPreviousPage();
-        }}>
-          <p>{'<'}</p>
-        </div>
-        <p className={s.textCurrentPage}>Page {store.data.Page} {totalDisplay}</p>
-        <div className={s.buttonPaginate + ' ' + (!store.nextPageAvailable() ? s.buttonPaginateDisabled : '')} onClick={() => {
-          if (store.nextPageAvailable())
-            store.loadNextPage();
-        }}>
-          <p>{'>'}</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  return <Paging page={store.data.Page} totalItems={store.data.TotalItems} limit={store.limit} nextPageAvailable={store.nextPageAvailable} previousPageAvailable={store.previousPageAvailable} loadNextPage={store.loadNextPage} loadPreviousPage={store.loadPreviousPage} />
 }
 
-export default Paging;
+export default InventoryPaging;
