9bd3b0d7221c05ced07539b750ed32d2a41647e2
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-04-20 12:37:03 -0400

    add csr support; add NextNProgress;

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Roblox CSS
 import '../styles/roblox/icons.css';
 import Navbar from '../components/navbar';
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Head from 'next/head';
 import Footer from '../components/footer';
 import dayjs from '../lib/dayjs';
+import NextNProgress from "nextjs-progressbar";
 import LoginModalStore from '../stores/loginModal';
 import AuthenticationStore from '../stores/authentication';
 import NavigationStore from '../stores/navigation';
@@ -15,6 +16,7 @@ import { getTheme, themeType } from '../services/theme';
 import MainWrapper from '../components/mainWrapper';
 import GlobalAlert from '../components/globalAlert';
 import ThumbnailStore from "../stores/thumbnailStore";
+import getFlag from "../lib/getFlag";
 
 // @ts-ignore
 if (process.browser) {
@@ -45,12 +47,14 @@ function RobloxApp({ Component, pageProps }) {
   // set theme:
   // jss globals apparently don't support parameters/props, so the only way to do a dynamic global style is to either append a <style> element, use setAttribute(), or append a css file.
   // @ts-ignore
-  const el = process.browser && document.getElementsByTagName('body');
-  if (el && el.length) {
-    const theme = getTheme();
-    const divBackground = theme === themeType.obc2016 ? 'url(/img/Unofficial/obc_theme_2016_bg.png) repeat-x #222224' : document.getElementById('theme-2016-enabled') ? '#e3e3e3' : '#fff';
-    el[0].setAttribute('style', 'background: ' + divBackground);
-  }
+  useEffect(() => {
+    const el = typeof window !== 'undefined' && document.getElementsByTagName('body');
+    if (el && el.length) {
+      const theme = getTheme();
+      const divBackground = theme === themeType.obc2016 ? 'url(/img/Unofficial/obc_theme_2016_bg.png) repeat-x #222224' : document.getElementById('theme-2016-enabled') ? '#e3e3e3' : '#fff';
+      el[0].setAttribute('style', 'background: ' + divBackground);
+    }
+  }, [pageProps]);
 
   return <div>
     <Head>
@@ -63,16 +67,17 @@ function RobloxApp({ Component, pageProps }) {
     <AuthenticationStore.Provider>
       <LoginModalStore.Provider>
         <NavigationStore.Provider>
-          <Navbar></Navbar>
+          <Navbar/>
         </NavigationStore.Provider>
       </LoginModalStore.Provider>
       <GlobalAlert />
       <MainWrapper>
+        {getFlag('clientSideRenderingEnabled', false) ? <NextNProgress options={{showSpinner: false}} color='#fff' height={2} /> : null}
         <ThumbnailStore.Provider>
           <Component {...pageProps} />
         </ThumbnailStore.Provider>
       </MainWrapper>
-      <Footer></Footer>
+      <Footer/>
     </AuthenticationStore.Provider>
   </div>
 }
