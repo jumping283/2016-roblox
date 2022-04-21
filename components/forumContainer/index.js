8ec4d78e6abbc221269632976c693734bda645de
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-04-21 12:16:54 -0400

    fix ad overflowing;

--- components/forumContainer/index.js
+++ components/forumContainer/index.js
@@ -3,23 +3,27 @@ import {createUseStyles} from "react-jss";
 
 const useStyles = createUseStyles({
   forumColumn: {
-    minWidth: '800px',
-  }
+  },
+  forumContainer: {
+    minWidth: '900px',
+  },
 });
 
 const ForumContainer = props => {
   const s = useStyles();
-  return <div className='container'>
-    <div className={'col-10 ' + s.forumColumn}>
-      <UserAdvertisement type={1} />
-      <div className='mt-4'>
-        <div className='bg-white pt-2 pb-2'>
-          {props.children}
+  return <div className={'container ' + s.forumContainer}>
+    <div className='row'>
+      <div className='col-10'>
+        <UserAdvertisement type={1} />
+        <div className='mt-4'>
+          <div className='bg-white pt-2 pb-2'>
+            {props.children}
+          </div>
         </div>
       </div>
-    </div>
-    <div className='col-2'>
-      <UserAdvertisement type={2} />
+      <div className='col-2'>
+        <UserAdvertisement type={2} />
+      </div>
     </div>
   </div>
 }
