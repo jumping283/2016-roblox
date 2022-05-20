73b1834c56d6699817ac6bddaf5108fa1c7cdb03
floatzeI <27046219+floatzeI@users.noreply.github.com>
2022-05-19 20:43:30 -0400

    add bc and tbc support;

--- components/bcOverlay/index.js
+++ components/bcOverlay/index.js
@@ -1,4 +1,6 @@
 import { createUseStyles } from "react-jss";
+import {useEffect, useState} from "react";
+import {getMembershipType} from "../../services/users";
 
 const useStyles = createUseStyles({
   overlay: {
@@ -9,9 +11,52 @@ const useStyles = createUseStyles({
   },
 });
 
+const iconsMap = {
+  1: `/img/overlay_bcOnly.png`,
+  2: `/img/overlay_tbcOnly.png`,
+  3: `/img/overlay_obcOnly.png`,
+  4: `/img/overlay_bcOnly.png`,
+};
+
+const Icon = props => {
+  const s= useStyles();
+  return <img className={s.overlay} src={iconsMap[props.type]} />;
+}
+
+let statusCache = {}
+let pendingCache = {}
 const BcOverlay = props => {
-  const s = useStyles();
-  return <img className={s.overlay} src={`/img/overlay_obcOnly.png`} />
+  const {id} = props;
+  const [type, setType] = useState(0);
+
+  useEffect(() => {
+    if (typeof window === undefined)
+      return;
+
+    if (statusCache[id] !== undefined) {
+      setType(statusCache[id]);
+      return;
+    }
+    if (pendingCache[id]) {
+      pendingCache[id].push((status) => {
+        setType(status);
+      });
+      return;
+    }
+    setType(0);
+    pendingCache[id] = [];
+    getMembershipType({userId: id}).then(d => {
+      setType(d);
+      pendingCache[id].forEach(v => v(d));
+      delete pendingCache[id];
+    })
+  }, [id]);
+
+  if (type === 0) {
+    return null;
+  }
+
+  return <Icon type={type} />
 }
 
 export default BcOverlay;
